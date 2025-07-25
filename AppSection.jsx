0e469cf72264c8d3ab5a0a5cbe89fa327f681881
FarWareApp <feras1198915.119@outlook.sa>
2025-07-25 10:27:51 +0200

    Delete AppSection.jsx

--- AppSection.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function AppSection({ id, title, description, color, icon, features, statusText }) {
-  return <section id={id}>/* AppSection JSX here */</section>;
-}
